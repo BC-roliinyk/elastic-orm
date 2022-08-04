ed49ec4d6b93a901922fbde8429eade71228233c
Roman Oliinyk <roman.oliinyk@bigcommerce.com>
2022-08-04 15:33:42 -0500

    add order query

--- index.php
+++ index.php
@@ -60,7 +60,54 @@ $customerSqsFields = [
     'phone.partial',
 ];
 
+$orderAddressSqsFields = [
+    'first_name^2',
+    'first_name.partial',
+    'last_name^2',
+    'last_name.partial',
+    'company^2',
+    'company.partial',
+    'street_1^2',
+    'street_1.partial',
+    'street_2^2',
+    'street_2.partial',
+    'city^2',
+    'city.partial',
+    'zip^2',
+    'zip.partial',
+    'country^2',
+    'country.partial',
+    'state^2',
+    'state.partial',
+    'email^2',
+    'email.partial',
+    'phone^2',
+    'phone.partial',
+];
 
+$orderProductSqsFields = [
+    'name^2',
+    'name.partial',
+    'description',
+    'sku^3',
+    'sku.partial',
+    'keywords^2',
+    'keywords.partial',
+    'brand_name^2',
+];
+
+$parentCustomerSqsFields = [
+    'company^2',
+    'company.partial',
+    'first_name^2',
+    'first_name.partial',
+    'last_name^2',
+    'last_name.partial',
+    'email^2',
+    'email.partial',
+    'phone^2',
+    'phone.partial',
+];
 
 
 $queryFactory = QueryFactory::getQueryObjectFactory();
@@ -82,7 +129,7 @@ $variantSqsQuery = $queryFactory
 //fill the variant bool query
 $boolVariantQuery->setMinimumShouldMatch(1);
 $boolVariantQuery->addQuery($variantSqsQuery);
-$boolVariantQuery->addQuery($storeFilter);
+$boolVariantQuery->addFilter($storeFilter);
 
 //set variant bool query as child
 $hasChildQuery = $queryFactory->getQueryObject('HasChildQuery')
@@ -116,8 +163,8 @@ $namePrefix = $queryFactory
 $boolQuery->setMinimumShouldMatch(1);
 $boolQuery->addShould($hasChildQuery);
 $boolQuery->addQuery($productSqsQuery);
-$boolQuery->addQuery($documentTypeFilter);
-$boolQuery->addQuery($storeFilter);
+$boolQuery->addFilter($documentTypeFilter);
+$boolQuery->addFilter($storeFilter);
 $boolQuery->addQuery($skuPrefix);
 $boolQuery->addQuery($keywordsPrefix);
 $boolQuery->addQuery($namePrefix);
@@ -157,7 +204,7 @@ foreach ($shippingAddressPrefixArray as $prefixName => $boost) {
         ->setPrefix($prefixName, $query, $boost);
     $shippingAddressBoolQuery->addQuery($prefixQuery);
 }
-$shippingAddressBoolQuery->addQuery($storeFilter);
+$shippingAddressBoolQuery->addFilter($storeFilter);
 
 
 
@@ -167,11 +214,11 @@ $hasShippingAdressChildQuery = $queryFactory->getQueryObject('HasChildQuery')
 $customerBoolQuery->addShould($hasShippingAdressChildQuery);
 
 //order child query
-$orderBoolQuery = $queryFactory->getQueryObject('BoolQuery');
+$childOrderBoolQuery = $queryFactory->getQueryObject('BoolQuery');
 //TODO orderBoolQuery->addShould(setFunctionScoreQuery)
-$orderBoolQuery->addQuery($storeFilter);
+$childOrderBoolQuery->addFilter($storeFilter);
 $orderHasChildQuery = $queryFactory->getQueryObject('HasChildQuery')
-    ->setQuery($orderBoolQuery)
+    ->setQuery($childOrderBoolQuery)
     ->setType('order');
 
 
@@ -202,18 +249,154 @@ foreach ($customerPrefixArray as $prefixName => $boost) {
         ->setPrefix($prefixName, $query, $boost);
     $customerBoolQuery->addShould($prefixQuery);
 }
-$customerBoolQuery->addQuery($storeFilter);
+$customerBoolQuery->addFilter($storeFilter);
 
 $documentTypeFilter = $queryFactory->getQueryObject('TermQuery');
 $documentTypeFilter->setTerm('document_type', 'customer');
 
-$customerBoolQuery->addQuery($documentTypeFilter);
+$customerBoolQuery->addFilter($documentTypeFilter);
 $customerBoolQuery->from(10);
 $customerBoolQuery->size(10);
 $customerBoolQuery->sort(['id']);
 
+//----------------------------------- build order query
 
+$orderBoolQuery = $queryFactory->getQueryObject('BoolQuery', 'master_document');
+$orderBoolQuery->setMinimumShouldMatch(1);
+//---child order_address
+$childOrderAddressBoolQuery = $queryFactory->getQueryObject('BoolQuery');
+$childOrderAddressBoolQuery->setMinimumShouldMatch(1);
+$childOrderAddressSqsQuery = $queryFactory
+    ->getQueryObject('SimpleQueryString')
+    ->setQuery($query)
+    ->setLenient(true)
+    ->setDefaultOperator('and')
+    ->setAnalyzeWildcard(true)
+    ->setFields($orderAddressSqsFields);
+$childOrderAddressBoolQuery->addShould($childOrderAddressSqsQuery);
+$childOrderAddressPrefixArray = [
+    'first_name.not_analyzed' => 1.5,
+    'last_name.not_analyzed' => 1.5,
+    'company.not_analyzed' => 1.5,
+    'street_1.not_analyzed' => 1.5,
+    'city.not_analyzed' => 1.5,
+    'country.not_analyzed' => 1.5,
+    'state.not_analyzed' => 1.5,
+    'email.not_analyzed' => 1.5,
+    'phone.not_analyzed' => 1.5,
+];
+foreach ($childOrderAddressPrefixArray as $prefixName => $boost) {
+    $prefixQuery = $queryFactory
+        ->getQueryObject('PrefixQuery')
+        ->setPrefix($prefixName, $query, $boost);
+    $childOrderAddressBoolQuery->addShould($prefixQuery);
+}
+$childOrderAddressBoolQuery->addFilter($storeFilter);
+
+$orderAddressHasChildQuery = $queryFactory->getQueryObject('HasChildQuery')
+    ->setQuery($childOrderAddressBoolQuery)
+    ->setType('order_address');
+
+$orderBoolQuery->addShould($orderAddressHasChildQuery); //adding should with child bool order_address
+
+//child---order_product
+$childOrderProductBoolQuery = $queryFactory->getQueryObject('BoolQuery');
+
+$orderProductHasChildQuery = $queryFactory->getQueryObject('HasChildQuery')
+    ->setQuery($childOrderProductBoolQuery)
+    ->setType('order_product');
+$childOrderProductBoolQuery->setMinimumShouldMatch(1);
+
+$childOrderProductSqsQuery = $queryFactory
+    ->getQueryObject('SimpleQueryString')
+    ->setQuery($query)
+    ->setDefaultOperator('and')
+    ->setAnalyzeWildcard(true)
+    ->setFields($orderProductSqsFields);
+
+$childOrderProductBoolQuery->addShould($childOrderProductSqsQuery);
+
+$prefixProductSkuQuery = $queryFactory
+    ->getQueryObject('PrefixQuery')
+    ->setPrefix('sku.not_analyzed', $query, 2);
+$prefixProductNameQuery = $queryFactory
+    ->getQueryObject('PrefixQuery')
+    ->setPrefix('name.not_analyzed', $query, 2);
+
+$childOrderProductBoolQuery->addShould($prefixProductSkuQuery);
+$childOrderProductBoolQuery->addShould($prefixProductNameQuery);
+
+$childOrderAddressBoolQuery->addShould($prefixQuery);
+$childOrderAddressBoolQuery->addFilter($storeFilter);
+
+$orderBoolQuery->addShould($orderAddressHasChildQuery);//adding should with child bool order_product
+
+//parent---customer
+$parentCustomerBoolQuery = $queryFactory->getQueryObject('BoolQuery');
+$parentCustomerBoolQuery->setMinimumShouldMatch(1);
+$parentCustomerSqsQuery = $queryFactory
+    ->getQueryObject('SimpleQueryString')
+    ->setQuery($query)
+    ->setDefaultOperator('and')
+    ->setAnalyzeWildcard(true)
+    ->setFields($parentCustomerSqsFields);
+
+$parentCustomerBoolQuery->addShould($parentCustomerSqsQuery);
+
+$parentCustomerPrefixArray = [
+    'company.not_analyzed' => 1.5,
+    'first_name.not_analyzed' => 1.5,
+    'last_name.not_analyzed' => 1.5,
+    'email.not_analyzed' => 1.5,
+    'phone.not_analyzed' => 1.5
+];
+foreach ($parentCustomerPrefixArray as $prefixName => $boost) {
+    $prefixQuery = $queryFactory
+        ->getQueryObject('PrefixQuery')
+        ->setPrefix($prefixName, $query, $boost);
+    $parentCustomerBoolQuery->addShould($prefixQuery);
+}
+
+$parentCustomerBoolQuery->addFilter($storeFilter);
+
+$customerHasParentQuery = $queryFactory->getQueryObject('HasParentQuery')
+    ->setQuery($parentCustomerBoolQuery)
+    ->setParentType('customer');
+
+$orderBoolQuery->addShould($customerHasParentQuery);
+
+
+$functionScoreQuery = $queryFactory->getQueryObject('FunctionScoreQuery');
+$functionBoolQuery = $queryFactory->getQueryObject('BoolQuery');
+$functionBoolQuery->setMinimumShouldMatch(1);
+$sqsQuery = $queryFactory
+    ->getQueryObject('SimpleQueryString')
+    ->setQuery($query)
+    ->setDefaultOperator('and')
+    ->setAnalyzeWildcard(true)
+    ->setFields($parentCustomerSqsFields);
+$functionBoolQuery->addShould($sqsQuery);
+
+$functionScoreQuery->setQuery($functionBoolQuery);
+$decayQuery = $queryFactory->getQueryObject('DecayFunction');
+$decayQuery->setType('gauss')->setField('created_date')->setOffset('45d')->setScale('135d')->setDecay(0.6);
+
+$functionScoreQuery->addFunction($decayQuery);
+
+$orderBoolQuery->addShould($functionScoreQuery);
+//TODO $orderBoolQuery->addShould(PostFilter)
+
+$orderBoolQuery->addFilter($storeFilter);
+
+$documentTypeFilter = $queryFactory->getQueryObject('TermQuery');
+$documentTypeFilter->setTerm('document_type', 'order');
+
+$orderBoolQuery->addFilter($documentTypeFilter);
+$orderBoolQuery->from(10);
+$orderBoolQuery->size(10);
+$orderBoolQuery->sort(['id']);
 
 
 var_dump($boolQuery->build());
 var_dump($customerBoolQuery->build());
+var_dump($orderBoolQuery->build());
